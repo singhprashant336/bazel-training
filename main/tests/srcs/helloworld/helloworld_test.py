01afac5cd88096b0552e448407ed83a4abc0a937
Prashant <prashantkumarsingh336@gmail.com>
2024-04-29 00:02:07 +0200

    Add new test cases

---
+++ main/tests/srcs/helloworld/helloworld_test.py
@@ -0,0 +1,6 @@
+ 
+
+def test_foo(capfd):
+    #foo()  # Writes "Hello World!" to stdout
+    #out, err = capfd.readouterr()
+    assert True #out == "Hello World!"
---
+++ main/tests/srcs/test1/test1.py
@@ -0,0 +1,6 @@
+import lxml
+
+def test_foo(capfd):
+    #foo()  # Writes "Hello World!" to stdout
+    #out, err = capfd.readouterr()
+    assert True #out == "Hello World!"
